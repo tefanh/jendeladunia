3c58421f6d53cae482c061e02d84e24ab2740217
Tefan Haetami <tefanhaetami87@gmail.com>
2021-08-19 09:15:48 +0700

    update: adding filter by region

--- src/app/ui/ui-maker.js
+++ src/app/ui/ui-maker.js
@@ -3,6 +3,7 @@ export function makeHeader() {
   header.style.position = 'sticky';
   header.style.top = '0';
   header.style.left = '0';
+  header.style.zIndex = '9';
 
   const navbar = document.createElement('app-navbar');
   navbar.setAttribute('title', 'Jendela Dunia');
@@ -28,4 +29,96 @@ export function makeToolbar() {
   const toolbar = document.createElement('div');
   toolbar.setAttribute('class', 'toolbar');
   return toolbar;
+}
+
+export function makeSearchForm() {
+  const searchForm = document.createElement('input');
+  searchForm.setAttribute('type', 'text');
+  searchForm.setAttribute('placeholder', 'search country');
+  searchForm.setAttribute('class', 'search-form');
+  return searchForm;
+}
+
+export function makeRegionForm() {
+  const regionForm = document.createElement('select');
+  regionForm.setAttribute('class', 'region-form');
+  return regionForm;
+}
+
+export function appendCountry(countries) {
+  return new Promise((resolve) => {
+    let i = 0;
+    const nodeList = [];
+    while (i < countries.length) {
+      const country = countries[i];
+      const ctrEl = document.createElement('div');
+      ctrEl.setAttribute('class', 'card');
+      ctrEl.setAttribute('id', country.name.replace(' ', '-').trim());
+      const figure = document.createElement('figure');
+      const img = document.createElement('img');
+      img.src = country.flag;
+      img.alt = country.nativeName;
+      img.style.width = '100%';
+      img.style.height = '92px';
+      const figcaption = document.createElement('figcaption');
+      figcaption.innerHTML = country.name;
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+      ctrEl.appendChild(figure);
+
+      ctrEl.onmouseenter = (e) => {
+        e.preventDefault();
+        figcaption.style.transform = 'translateY(0)';
+      };
+
+      ctrEl.onmouseleave = (e) => {
+        e.preventDefault();
+        figcaption.style.transform = 'translateY(100%)';
+      };
+      nodeList.push(ctrEl);
+      i++;
+    }
+    resolve(nodeList);
+  });
+}
+
+export function makeModal() {
+  const modal = document.createElement('div');
+  modal.setAttribute('class', 'modal');
+  const modalContent = document.createElement('div');
+  modalContent.setAttribute('class', 'modal-content');
+  const modalHeader = document.createElement('div');
+  modalHeader.setAttribute('class', 'modal-header');
+  const modalBody = document.createElement('div');
+  modalBody.setAttribute('class', 'modal-body');
+  const modalFooter = document.createElement('div');
+  modalFooter.setAttribute('class', 'modal-footer');
+
+  const spanClose = document.createElement('span');
+  spanClose.setAttribute('class', 'close');
+  spanClose.setAttribute('id', 'closeModalButton');
+  spanClose.innerHTML = '&times;';
+
+  const modalHeaderTitle = document.createElement('h2');
+  modalHeaderTitle.setAttribute('id', 'modalHeaderTitle');
+
+  modalHeader.appendChild(spanClose);
+  modalHeader.appendChild(modalHeaderTitle);
+
+  const modalBodyText = document.createElement('p');
+  modalBodyText.innerText = 'Lorem ipsum dolor sit amet';
+
+  modalBody.appendChild(modalBodyText);
+
+  const modalFooterText = document.createElement('h3');
+  modalFooterText.innerText = 'Modal Footer';
+
+  modalFooter.appendChild(modalFooterText);
+
+  modalContent.appendChild(modalHeader);
+  modalContent.appendChild(modalBody);
+  modalContent.appendChild(modalFooter);
+
+  modal.appendChild(modalContent);
+  return modal;
 }
--- src/index.js
+++ src/index.js
@@ -2,66 +2,103 @@ import './style.css';
 import { getAllCountries } from './app/api/restcountries';
 import Navbar from './app/ui/navbar/navbar';
 import Footer from './app/ui/footer/footer';
-import { makeHeader, makeMain, makeFooter } from './app/ui/ui-maker';
+import {
+  makeHeader,
+  makeMain,
+  makeFooter,
+  makeToolbar,
+  appendCountry,
+  makeSidebar,
+  makeSearchForm,
+  makeRegionForm,
+  makeModal,
+} from './app/ui/ui-maker';
 
 customElements.define('app-navbar', Navbar);
 customElements.define('app-footer', Footer);
 
 const root = document.querySelector('div#root');
-const [header, main, footer] = [makeHeader(), makeMain(), makeFooter()];
+const [header, main, footer, toolbar, searchForm, regionForm, modal] = [
+  makeHeader(),
+  makeMain(),
+  makeFooter(),
+  makeToolbar(),
+  makeSearchForm(),
+  makeRegionForm(),
+  makeModal(),
+];
 let countries = [];
 let filteredCountries = [];
+let regions = [];
+let searchValue = '';
+let regionValue = '';
+
 const countryCardContainer = document.createElement('div');
 countryCardContainer.setAttribute('class', 'card-container');
 
-function appendCountry() {
-  countryCardContainer.innerHTML = ``;
-  for (const country of filteredCountries) {
-    const ctrEl = document.createElement('div');
-    ctrEl.setAttribute('class', 'card');
-    const figure = document.createElement('figure');
-    const img = document.createElement('img');
-    img.src = country.flag;
-    img.alt = country.nativeName;
-    img.style.width = '100%';
-    img.style.height = '92px';
-    const figcaption = document.createElement('figcaption');
-    figcaption.innerHTML = country.name;
-    figure.appendChild(img);
-    figure.appendChild(figcaption);
-    ctrEl.appendChild(figure);
-
-    ctrEl.onmouseenter = (e) => {
-      e.preventDefault();
-      figcaption.style.transform = 'translateY(0)';
-    };
-
-    ctrEl.onmouseleave = (e) => {
-      e.preventDefault();
-      figcaption.style.transform = 'translateY(100%)';
-    };
-
-    countryCardContainer.appendChild(ctrEl);
-  }
+function resetCountryCardContainer() {
+  countryCardContainer.innerHTML = '';
+  appendCountry(filteredCountries).then((nodeList) => {
+    nodeList.map((li) => {
+      li.onclick = (e) => {
+        e.preventDefault();
+        document.getElementById('modalHeaderTitle').innerText =
+          li.getAttribute('id');
+        modal.style.display = 'block';
+      };
+      countryCardContainer.appendChild(li);
+    });
+  });
 }
 
 function initSearchForm() {
-  const searchForm = document.createElement('input');
-  searchForm.setAttribute('type', 'text');
-  searchForm.setAttribute('placeholder', 'search country');
   searchForm.onkeyup = (e) => {
     e.preventDefault();
-    filteredCountries =
-      e.target.value === ''
-        ? countries
-        : countries.filter((country) =>
-            String(country.name.toLowerCase()).includes(
-              e.target.value.toLowerCase(),
-            ),
-          );
-    appendCountry();
+    searchValue = e.target.value;
+    filteredCountries = countries.filter(
+      (country) =>
+        String(country.name.toLowerCase()).includes(
+          searchValue.toLowerCase(),
+        ) &&
+        String(country.region.toLowerCase()).includes(
+          regionValue.toLowerCase(),
+        ),
+    );
+    resetCountryCardContainer();
   };
-  main.querySelector('div.main-content').appendChild(searchForm);
+  toolbar.appendChild(searchForm);
+}
+
+function initFilterByRegionForm() {
+  for (const region of regions) {
+    const option = document.createElement('option');
+    option.value = region === 'All' ? '' : region;
+    option.text = region;
+    regionForm.appendChild(option);
+  }
+  regionForm.onchange = (e) => {
+    e.preventDefault();
+    regionValue = e.target.value;
+    filteredCountries = countries.filter(
+      (country) =>
+        String(country.name.toLowerCase()).includes(
+          searchValue.toLowerCase(),
+        ) &&
+        String(country.region.toLowerCase()).includes(
+          regionValue.toLowerCase(),
+        ),
+    );
+    resetCountryCardContainer();
+  };
+  toolbar.appendChild(regionForm);
+}
+
+function initModal() {
+  document.body.appendChild(modal);
+  document.getElementById('closeModalButton').onclick = (e) => {
+    e.preventDefault();
+    modal.style.display = 'none';
+  };
 }
 
 onload = async () => {
@@ -70,11 +107,26 @@ onload = async () => {
   root.appendChild(footer);
   //   get all country
   countries = await getAllCountries();
-  console.log(countries);
+  regions = countries
+    .map((country) => country.region)
+    .filter(
+      (thing, index, self) =>
+        thing.length > 0 &&
+        index ===
+          self.findIndex((t) => t.toLowerCase() === thing.toLowerCase()),
+    );
+  regions.splice(0, 0, 'All');
+  regionValue = '';
   filteredCountries = [...countries];
+  // init toolbar
+  main.querySelector('div.main-content').appendChild(toolbar);
   // init search form
   initSearchForm();
+  // init filter by region form
+  initFilterByRegionForm();
+  // init modal
+  initModal();
   // init ul list country
   main.querySelector('div.main-content').appendChild(countryCardContainer);
-  appendCountry();
+  resetCountryCardContainer();
 };
